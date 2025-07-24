ed53bae0f9c52687857e49ace67902df5d55b415
AkunuriPravalika <akunuripravalika4@gmail.com>
2025-07-24 12:12:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 import streamlit as st
 import requests
 
-BASE_URL = "http://localhost:5000"  # Replace with Render URL when deployed
+BASE_URL = "http://127.0.0.1:5000/"  # Replace with Render URL when deployed
 
 st.set_page_config(page_title="User Manager", layout="centered")
 st.title("📋 User Management Dashboard")
